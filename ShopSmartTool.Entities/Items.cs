d60777579e5c1cf60f2f2cb3e137b1b50e362dfa
unknown <tulsi.kiraula111@gmail.com>
2019-05-03 23:33:49 +0530

    commit message

---
+++ ShopSmartTool.Entities/Items.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShopSmartTool.Entities
+{
+    /// <summary>
+    /// The Item enitity class , which contains the description of the item
+    /// </summary>
+    public class Items
+    {
+        #region Members
+        /// <summary>
+        /// The item id to uniquely identify the item
+        /// </summary>
+        public int ItemId { get; set; }
+        /// <summary>
+        /// The name of the item
+        /// </summary>
+        public string ItemName { get; set; }
+        /// <summary>
+        /// The price of the item
+        /// </summary>
+        public int Price { get; set; }
+        /// <summary>
+        /// The offer available on the item
+        /// </summary>
+        public string Offer { get; set; }
+
+        #endregion Members
+    }
+}
---
+++ ShopSmartTool/Common/NotificationChangeHelper.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace GoShopping.App.Common
+{
+    public class PropertyChangeNotifier : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
